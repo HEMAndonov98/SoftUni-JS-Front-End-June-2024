f1884b3ca16178f5c3364ad84b4696dd0e536bfe
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-08-12 00:25:56 +0300

    Exercise 5 Completed 100/100 Judge score

--- 10. JS DOM Introduction Exercise/05. Table - Search Engine/solution.js
+++ 10. JS DOM Introduction Exercise/05. Table - Search Engine/solution.js
@@ -2,7 +2,19 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      const inputText = document.getElementById('searchField');
+      const tableCells = document.querySelectorAll('table>tbody tr');
 
+      const tableRowsArray = Array.from(tableCells);
+
+      for (const row of tableRowsArray) {
+         row.classList.remove('select');
+         for (const cell of Array.from(row.children)) {
+            if (cell.textContent.includes(inputText.value)) {
+               row.classList.add('select');
+            }
+         }
+      }
+      inputText.value = '';
    }
 }
