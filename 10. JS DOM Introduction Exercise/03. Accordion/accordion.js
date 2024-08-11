702d221785153a295e90511c7e58dd5ab367ad27
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-08-11 22:19:56 +0300

    Exercise 3 Completed 100/100 Judge Score

--- 10. JS DOM Introduction Exercise/03. Accordion/accordion.js
+++ 10. JS DOM Introduction Exercise/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    const buttonElement = document.getElementsByClassName('button')[0];
+    const hiddenElement = document.getElementById('extra');
+
+    if (buttonElement.textContent === 'More') {
+        buttonElement.textContent = 'Less';
+        hiddenElement.style.display = 'block';
+    } else {
+        buttonElement.textContent = 'More';
+        hiddenElement.style.display = 'none';
+    }
 }
