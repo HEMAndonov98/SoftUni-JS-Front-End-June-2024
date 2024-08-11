124824aef4aaec57c591efccdb8fef88571f95d2
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-08-11 23:28:22 +0300

    Exercise 4 Completed 100/100 Judge score

--- 10. JS DOM Introduction Exercise/04. Search in List/search.js
+++ 10. JS DOM Introduction Exercise/04. Search in List/search.js
@@ -1,3 +1,22 @@
 function search() {
-   // TODO
+   const towns = document.getElementById('towns').children;
+   const inputText = document.getElementById('searchText').value;
+   const result = document.getElementById('result');
+   let matches = 0;
+
+   const townsArray = Array.from(towns);
+
+   for (const townItem of townsArray) {
+
+      townItem.style.fontWeight = 'normal';
+      townItem.style.textDecoration = 'none';
+
+      if (townItem.textContent.includes(inputText)) {
+         townItem.style.fontWeight = 'bold';
+         townItem.style.textDecoration = 'underline';
+         matches++;
+      }
+   }
+
+   result.textContent = `${matches} matches found`;
 }
