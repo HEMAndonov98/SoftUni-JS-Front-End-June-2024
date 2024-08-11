8e1646434ec4654a5a7744724ed7a13d7a3b1ae3
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-08-11 22:08:31 +0300

    Exercise 2 Completed 100/100 Judge score

--- 10. JS DOM Introduction Exercise/02. Pascal or Camel Case/solution.js
+++ 10. JS DOM Introduction Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,34 @@
 function solve() {
-  //TODO...
+  const text = document.getElementById('text').value;
+  const namingConvention = document.getElementById('naming-convention').value;
+  const resultElement = document.getElementById('result');
+  const textArray = text.split(' ').map(x => x.toLowerCase());
+  const resultArr = [];
+
+
+  if (namingConvention === 'Camel Case') {
+
+    resultArr.push(textArray[0]);
+
+    for (let i = 1; i < textArray.length; i++) {
+      let element = textArray[i];
+      element = element.replace(element[0], element[0].toUpperCase());
+
+      resultArr.push(element);
+    }
+    resultElement.textContent = resultArr.join('');
+  } else if (namingConvention === 'Pascal Case') {
+
+    for (let i = 0; i < textArray.length; i++) {
+      let element = textArray[i];
+      element = element.replace(element[0], element[0].toUpperCase());
+
+      resultArr.push(element);
+    }
+    resultElement.textContent = resultArr.join('');
+  } else {
+    resultElement.textContent = 'Error!';
+    return;
+  }
+
 }
