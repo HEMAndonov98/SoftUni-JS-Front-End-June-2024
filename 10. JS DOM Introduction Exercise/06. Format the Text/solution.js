9d913e3ecaa954144badc8ae82d05ecce6f22d08
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-08-12 19:34:48 +0300

    Exerise 6 60/100 Judge

--- 10. JS DOM Introduction Exercise/06. Format the Text/solution.js
+++ 10. JS DOM Introduction Exercise/06. Format the Text/solution.js
@@ -1,3 +1,26 @@
 function solve() {
-  //TODO
+  const inputText = document.getElementById('input').value;
+  const outputNode = document.getElementById('output');
+
+  if (inputText === '') { return; }
+
+  const sentances = inputText.split('.').map(sentance => sentance.trim());
+  const paraCount = Math.ceil(sentances.length / 3);
+
+  for (let i = 0; i < paraCount; i++) {
+    const para = document.createElement('p');
+    let paraText = '';
+
+    for (let j = 0; j < 3; j++) {
+      if (sentances.length > 0) {
+        const sentance = sentances.shift().trim();
+        if (sentance.length > 0) {
+          paraText = paraText.concat(' ', `${sentance}.`);
+        }
+      }
+
+    }
+    para.textContent = paraText.trim();
+    outputNode.appendChild(para);
+  }
 }
