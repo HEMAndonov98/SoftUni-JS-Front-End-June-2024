0f8a1f24dde23081202c85efb74f71d85ae70fa7
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-08-16 21:34:07 +0300

    Exercise 5 completed 100/100 Judge score

--- 11. JS DOM Manipulation&Events Lab/05. Highlight-Active/app.js
+++ 11. JS DOM Manipulation&Events Lab/05. Highlight-Active/app.js
@@ -1,3 +1,18 @@
 function focused() {
-    console.log('TODO:...');
+    const inputEls = [...document.querySelectorAll('input')];
+
+    const clearClasses = (inputElsArray) => {
+        for (const inputEl of inputElsArray) {
+            inputEl.parentNode.classList.remove('focused');
+        }
+    }
+
+    inputEls.map((inputEl) => {
+        inputEl.addEventListener('focus', (el) => {
+
+            clearClasses(inputEls);
+            el.currentTarget.parentNode.classList.add('focused');
+
+        });
+    })
 }
