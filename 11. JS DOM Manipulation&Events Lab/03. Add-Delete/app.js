d2b128d282131b5f8f4d77e2390e3cd3747d06ae
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-08-16 20:23:17 +0300

    Exercise 3 completed 100/100 Judge score

--- 11. JS DOM Manipulation&Events Lab/03. Add-Delete/app.js
+++ 11. JS DOM Manipulation&Events Lab/03. Add-Delete/app.js
@@ -1,3 +1,23 @@
 function addItem() {
-    //TODO...
+    const inputField = document.getElementById('newItemText');
+    const listElement = document.getElementById('items');
+
+    const createListItem = (input) => {
+        const newListItem = document.createElement('li');
+        newListItem.textContent = input;
+
+        const deleteLink = document.createElement('a');
+        deleteLink.textContent = '[Delete]';
+        deleteLink.setAttribute('href', '#');
+
+        newListItem.appendChild(deleteLink)
+        deleteLink.addEventListener('click', (e) => {
+            e.currentTarget.parentNode.remove();
+        })
+
+        return newListItem;
+    }
+
+    listElement.append(createListItem(inputField.value));
+    inputField.value = '';
 }
