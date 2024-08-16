4daae67c06195cee4e761a0695d2cb9df72a6446
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-08-16 19:58:00 +0300

    Exercise 2 completed 100/100 Judge score

--- 11. JS DOM Manipulation&Events Lab/02. Delete-From-Table/app.js
+++ 11. JS DOM Manipulation&Events Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,17 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const tBodyEl = document.querySelector('table>tbody');
+    const inputField = document.querySelector('input');
+
+    let isDeleted = false;
+    [...tBodyEl.children].map((trEl) => {
+        const emailCell = trEl.children[1];
+        if (inputField.value === emailCell.textContent) {
+            tBodyEl.removeChild(trEl);
+            isDeleted = true;
+        }
+    });
+
+    const resultDiv = document.getElementById('result');
+    resultDiv.textContent = isDeleted ? 'Deleted.' : 'Not found.';
+    inputField.value = '';
 }
