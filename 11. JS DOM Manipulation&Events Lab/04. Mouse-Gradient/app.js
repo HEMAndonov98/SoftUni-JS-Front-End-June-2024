b6c5e493e26c343e66d71f5af7c202b6dcea4c30
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-08-16 20:57:09 +0300

    Exercise 4 completed 100/100 Jdge score

--- 11. JS DOM Manipulation&Events Lab/04. Mouse-Gradient/app.js
+++ 11. JS DOM Manipulation&Events Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,13 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const divEl = document.getElementById('gradient');
+    const resultDiv = document.getElementById('result');
+
+    divEl.addEventListener('mousemove', (e) => {
+        const targetWidth = e.currentTarget.clientWidth;
+        const currentMouseLocation = e.offsetX;
+
+        const percentageCalculation = Math.floor((currentMouseLocation / targetWidth) * 100);
+        resultDiv.textContent = `${percentageCalculation}%`
+
+    })
 }
