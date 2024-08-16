6c263762e402c2461c61d97805a86fb3bcddd984
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-08-16 21:54:08 +0300

    Exercise 6 completed 100/100 Judge score

--- 11. JS DOM Manipulation&Events Lab/06. Dynamic-Validation/app.js
+++ 11. JS DOM Manipulation&Events Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    console.log('TODO:...');
+    const inputField = document.getElementById('email');
+    inputField.addEventListener('change', (e) => {
+        const inputValue = e.currentTarget.value;
+        const pattern = /[a-z]+@[a-z]+.[a-z]+/;
+
+        if (inputValue.match(pattern) === null) {
+            e.currentTarget.classList.add('error');
+        } else {
+            e.currentTarget.classList.remove('error');
+        }
+    })
 }
