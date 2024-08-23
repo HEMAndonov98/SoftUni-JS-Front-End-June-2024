cb62466638fdd2623812b2b3aa4eb68ba4518a2b
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-08-23 10:33:05 +0300

    Exercise 1 completed

--- 13. JS HTTP&REST Lab/02. GitHub-Repos/scripts/app.js
+++ 13. JS HTTP&REST Lab/02. GitHub-Repos/scripts/app.js
@@ -1,3 +1,19 @@
 function loadRepos() {
-   console.log("TODO...");
+	const resEl = document.getElementById("res");
+	const gitHubRepoAPI = "https://api.github.com/users/testnakov/repos";
+
+	const fetchRepoBodyRaw = (url) =>
+		new Promise((resolve, reject) => {
+			fetch(url).then((response) => {
+				if (response.ok) {
+					resolve(response.text());
+				} else {
+					reject(response.status);
+				}
+			});
+		});
+
+	fetchRepoBodyRaw(gitHubRepoAPI)
+		.then((resText) => (resEl.textContent = resText))
+		.catch((resStat) => (resEl.textContent = `Error ${resStat}`));
 }
