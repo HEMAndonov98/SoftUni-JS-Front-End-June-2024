6c6e2c6577374f47ab8335a653d3c4b32dfe443e
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-08-26 18:13:44 +0300

    Exercise 4 completed 100/100 Judge score

--- 12. JS DOM Manipulation&Events Exercise/04. Fill-Dropdown/dropdown.js
+++ 12. JS DOM Manipulation&Events Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,20 @@
 function addItem() {
-    console.log('TODO:...');
+	const optionTextEl = document.getElementById("newItemText");
+	const optionValueEl = document.getElementById("newItemValue");
+
+	function createOption(opText, opValue) {
+		const optionEl = document.createElement("option");
+
+		optionEl.value = opValue;
+		optionEl.textContent = opText;
+		return optionEl;
+	}
+
+	const newOption = createOption(optionTextEl.value, optionValueEl.value);
+
+	const selectMenuE = document.getElementById("menu");
+	selectMenuE.appendChild(newOption);
+
+	optionTextEl.value = "";
+	optionValueEl.value = "";
 }
