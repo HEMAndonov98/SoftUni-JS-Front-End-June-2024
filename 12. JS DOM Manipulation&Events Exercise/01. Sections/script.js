0e1a50ad9f60b29840c36abecdc1c1c21d24dd6b
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-08-26 12:45:44 +0300

    Exercise 1 completed 100/100 Judge Score

--- 12. JS DOM Manipulation&Events Exercise/01. Sections/script.js
+++ 12. JS DOM Manipulation&Events Exercise/01. Sections/script.js
@@ -1,3 +1,35 @@
 function create(words) {
-   console.log('TODO:...');
+	const contentEl = document.getElementById("content");
+	// Create a method to create the required elements
+	function createSection() {
+		const divEl = document.createElement("div");
+		const pEl = document.createElement("p");
+
+		pEl.id = "section-text";
+		pEl.style.display = "none";
+		divEl.appendChild(pEl);
+		return divEl;
+	}
+
+	// append the given text to the elements
+	function appendWord(DomNode, word) {
+		const pEl = DomNode.children[0];
+
+		pEl.textContent = word;
+	}
+	// Create event listeners for every div
+	function displayEvent(e) {
+		const pEl = e.target.children[0];
+		pEl.style.display = pEl.style.display === "none" ? "block" : "none";
+	}
+
+	// Try to create event delegation pattern
+	contentEl.addEventListener("click", displayEvent);
+
+	words.map((word) => {
+		const section = createSection();
+		appendWord(section, word);
+
+		contentEl.appendChild(section);
+	});
 }
