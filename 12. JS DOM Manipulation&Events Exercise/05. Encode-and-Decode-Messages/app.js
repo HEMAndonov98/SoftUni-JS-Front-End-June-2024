5b31484e108c9885cd1f21e30b09823c4a8bc770
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-08-26 18:49:34 +0300

    Exercise 5 completed 100/100 Judge Score

--- 12. JS DOM Manipulation&Events Exercise/05. Encode-and-Decode-Messages/app.js
+++ 12. JS DOM Manipulation&Events Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,43 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+	const buttons = [...document.querySelectorAll("#main>div>button")];
+
+	buttons[0].addEventListener("click", encodeText);
+	buttons[1].addEventListener("click", decodeText);
+	function encodeText() {
+		const textEl = document.querySelector("#main>div>textarea");
+		const text = textEl.value;
+
+		let hiddenText = "";
+
+		for (let i = 0; i < text.length; i++) {
+			const charCode = text.charCodeAt(i);
+
+			hiddenText += String.fromCharCode(charCode + 1);
+		}
+
+		sendHidden(hiddenText);
+		textEl.value = "";
+	}
+
+	function sendHidden(hiddenText) {
+		const receiver = document.querySelector(
+			"#main>div:last-child>textarea"
+		);
+
+		receiver.value = hiddenText;
+	}
+
+	function decodeText() {
+		const textEl = document.querySelector("#main>div:last-child>textarea");
+		const encodedText = textEl.value;
+
+		let decodedText = "";
+		for (let i = 0; i < encodedText.length; i++) {
+			const charCode = encodedText.charCodeAt(i);
+
+			decodedText += String.fromCharCode(charCode - 1);
+		}
+
+		textEl.value = decodedText;
+	}
 }
