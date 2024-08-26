c4194176471406aa4fa937b2a6589e8a98e5bc5a
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-08-26 18:01:09 +0300

    Exercise 3 completed 100/100 Judge Score

--- 12. JS DOM Manipulation&Events Exercise/03. Locked-Profile/app.js
+++ 12. JS DOM Manipulation&Events Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,45 @@
 function lockedProfile() {
-    console.log('TODO...')
+	document.getElementById("main").addEventListener("click", (e) => {
+		const target = e.target;
+
+		if (target.tagName === "BUTTON" && target.textContent === "Show more") {
+			handleShowMore(e);
+		} else if (
+			target.tagName === "BUTTON" &&
+			target.textContent === "Hide it"
+		) {
+			handleHideIt(e);
+		}
+	});
+
+	function checkUnlocked(targetParent) {
+		const isChecked = targetParent.children[4].checked;
+		return isChecked;
+	}
+
+	function showHidden(eventTarget) {
+		const divHiddenEl = eventTarget.parentElement.querySelector("div");
+		divHiddenEl.style.display = "block";
+		eventTarget.textContent = "Hide it";
+	}
+
+	function hideData(eventTarget) {
+		const divHiddenEl = eventTarget.parentElement.querySelector("div");
+		divHiddenEl.style.display = "none";
+		eventTarget.textContent = "Show more";
+	}
+
+	function handleShowMore(e) {
+		const isUnlocked = checkUnlocked(e.target.parentElement);
+		if (isUnlocked) {
+			showHidden(e.target);
+		}
+	}
+
+	function handleHideIt(e) {
+		const isUnlocked = checkUnlocked(e.target.parentElement);
+		if (isUnlocked) {
+			hideData(e.target);
+		}
+	}
 }
